b0b995a49f45970e749b2efe0ec46873407057ef
root <root@pop-os.localdomain>
2024-02-21 02:45:02 +0530

    renamed App2> App

--- btc-fee-charter/index.tsx
+++ btc-fee-charter/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App2';
+import App from './App';
 
 const rootElement = document.getElementById('root');
 createRoot(rootElement!).render(<App />);
